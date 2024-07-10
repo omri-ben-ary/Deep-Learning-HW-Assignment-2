3e413e605d609b58aca2ad079f1b57d8af975d18
Yuval Rosman <yuvalrosman@cs.technion.ac.il>
2024-07-10 22:19:57 +0300

    nothing is working

--- hw2/answers.py
+++ hw2/answers.py
@@ -10,27 +10,31 @@ math (delimited with $$).
 
 part1_q1 = r"""
 **Your answer:**
+1.
 
+A. The tensor shape is in correlation with the input, output and batch size. We want to calculate for each output, the amount of input derivetives, and of course, take the batch size into account. In total we will get [batch_size, out_features, in_features] =  [64, 512 , 1024]
 
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
+B. No, the Jacobian matrix is not necessarily sparse as this is a fully connected layer meaning that every input is not trivialy 0. Each ouput is a function of every input and for it to be sparse most input element need to be zero and this is not the case for every input.
+
+C. In order to calculate the downstream gradient w.r.t $\delta\mat{X}$ we do not need to explicitly matrialize the Jacobian matrix. Instead, we can use the following calculation: $\delta\mat{X} =\pderiv{L}{\mat{Y}}W^T$ which is much easier to calculate. We can argue that $W^T$ is the Jacobian matrix but we don't actually need to derive it, as it is given to us explicitly by the model paramters.
+
+2.
+
+A. The tensor shape is in correlation with the weight matrix shape and the output size. Using the same logic above, apart from the batch size which is irrelavent here because W shape is not dependant on the batch size. We get $[512, 512\cdot1024]$.
+
+B. Yes, the Jacobian matrix is sparse. Each Y element is depandent on the corresponding column in W matrix. For example, the first Y element is depandent on the first row because when we do the matrix multiplication $\mat{W}\cdot \mat{X},$ $Y_1$ is multiplied only by $W_{11}$ in the first row, for the second row $Y_1$ is multiplied by $W_{21}$ only etc. Therfore, the rest of the elements will be zero ($W_{ij}, j\ne1$) for the first Y element.
+
+C. As mentioned in question 1.c , we do not need to calculate materialize the Jacobian. The same reasons are apllied in this section, as we only need to calculate $\delta\mat{X} =\pderiv{L}{\mat{Y}}W^T$.
 
 """
 
 part1_q2 = r"""
 **Your answer:**
 
+Yes.
+While using back-propagation is todays standard in training neural networks, it is not required in order to just train the network. However, if it is our goal to train the network with decent-based optimization, it is **required**.
+If we weren't to use back-propagation, we would have to do a forward pass in order to calculate every partial derivative, which is highly inefficient.
 
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
 
 """
 
@@ -92,14 +96,9 @@ def part2_dropout_hp():
 
 part2_q1 = r"""
 **Your answer:**
+1. Yes they do match our expectations. We can see that when we use 0.4 dropout rate, we "force" the model to use all it's weights to make a prediction. When we don't use dropout, the model can rely heavily on certain weights that can lead to biases in prediction. In some sense, dropout acts as a regulrization mechanism to help the model generalize, this is why accuracy is better when using 0.4 dropour rate.
 
-
-Write your answer using **markdown** and $\LaTeX$:
-```python
-# A code block
-a = 2
-```
-An equation: $e^{i\pi} -1 = 0$
+2. We notice that when the dropout rate is too big (like the case of 0.8) the model practicaly does not learn as we drop most weights. It can be seen in the graph that accuracy is very low accordingly.
 
 """
 
