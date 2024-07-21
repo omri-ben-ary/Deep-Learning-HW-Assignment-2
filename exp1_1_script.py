0b1e809341106625c485720b9721b903bd77ea8d
Omri Ben-Ary <omri@cs.technion.ac.il>
2024-07-21 23:17:52 +0300

    add mor exps

--- exp1_1_script.py
+++ exp1_1_script.py
@@ -1,39 +1,39 @@
 import subprocess
 
-#exp1_1
-# Define the range of values for the parameters
-L_values = [2, 4, 8, 16]
-K_values = [32, 64]
-# P_values = range(1,9)
-# H_values = [32]
+# #exp1_1
+# # Define the range of values for the parameters
+# L_values = [2, 4, 8, 16]
+# K_values = [32, 64]
+# # P_values = range(1,9)
+# # H_values = [32]
 
-# Iterate through all combinations of parameters
-for K in K_values:
-    for L in L_values:
-        # for P in P_values:
-            # for H in H_values:
-        # Construct the command
-        command = [
-            "srun",
-            "-c", "2",
-            "--gres=gpu:1",
-            "--pty",
-            "python",
-            "-m", "hw2.experiments",
-            "run-exp",
-            "-n", "exp1_1",
-            "-K", str(K),
-            "-L", str(L),
-            "-H", "100",
-            "--early-stopping", "8",
-            "-M", "cnn",
-            "-s", "42",
-            "-P", str(min(5, int(L/2)))
-        ]
+# # Iterate through all combinations of parameters
+# for K in K_values:
+#     for L in L_values:
+#         # for P in P_values:
+#             # for H in H_values:
+#         # Construct the command
+#         command = [
+#             "srun",
+#             "-c", "2",
+#             "--gres=gpu:1",
+#             "--pty",
+#             "python",
+#             "-m", "hw2.experiments",
+#             "run-exp",
+#             "-n", "exp1_1",
+#             "-K", str(K),
+#             "-L", str(L),
+#             "-H", "100",
+#             "--early-stopping", "8",
+#             "-M", "cnn",
+#             "-s", "42",
+#             "-P", str(min(5, int(L/2)))
+#         ]
             
-        # Execute the command
-        print("Running command: {}".format(' '.join(command)))
-        subprocess.call(command)
+#         # Execute the command
+#         print("Running command: {}".format(' '.join(command)))
+#         subprocess.call(command)
 
 
 
@@ -113,74 +113,74 @@ for K in K_values:
 
 
 
-# #exp1_4_1
-# # Define the range of values for the parameters
-# L_values = [8,16,32]
-# K_values = [32]
-# # P_values = [2, 3]
-# # H_values = [100, 200]
+#exp1_4_1
+# Define the range of values for the parameters
+L_values = [8,16,32]
+K_values = [32]
+# P_values = [2, 3]
+# H_values = [100, 200]
 
-# # Iterate through all combinations of parameters
-# for K in K_values:
-#     for L in L_values:
-#         # for P in P_values:
-#             # for H in H_values:
-#                 # Construct the command
-#         command = [
-#             "srun",
-#             "-c", "2",
-#             "--gres=gpu:1",
-#             "--pty",
-#             "python",
-#             "-m", "hw2.experiments",
-#             "run-exp",
-#             "-n", "exp1_4",
-#             "-K", str(K),
-#             "-L", str(L),
-#             "-H", "100",
-#             "--early-stopping", "8",
-#             "-M", "resnet",
-#             "-s", "42",
-#             "-P", str(min(5, int(L/2)))  
-#         ]
+# Iterate through all combinations of parameters
+for K in K_values:
+    for L in L_values:
+        # for P in P_values:
+            # for H in H_values:
+                # Construct the command
+        command = [
+            "srun",
+            "-c", "2",
+            "--gres=gpu:1",
+            "--pty",
+            "python",
+            "-m", "hw2.experiments",
+            "run-exp",
+            "-n", "exp1_4",
+            "-K", str(K),
+            "-L", str(L),
+            "-H", "100",
+            "--early-stopping", "8",
+            "-M", "resnet",
+            "-s", "42",
+            "-P", str(min(5, int(L/2)))  
+        ]
         
-#         # Execute the command
-#         print("Running command: {}".format(' '.join(command)))
-#         subprocess.call(command)
+        # Execute the command
+        print("Running command: {}".format(' '.join(command)))
+        subprocess.call(command)
 
 
 
-# #exp1_4_2
-# # Define the range of values for the parameters
-# L_values = [2,4,8]
-# K_values = [32]
-# # P_values = [2, 3]
-# # H_values = [100, 200]
+#exp1_4_2
+# Define the range of values for the parameters
+L_values = [2,4,8]
+K_values = [[64, 128, 256]]
+# P_values = [2, 3]
+# H_values = [100, 200]
 
-# # Iterate through all combinations of parameters
-# for K in K_values:
-#     for L in L_values:
-#         # for P in P_values:
-#             # for H in H_values:
-#                 # Construct the command
-#         command = [
-#             "srun",
-#             "-c", "2",
-#             "--gres=gpu:1",
-#             "--pty",
-#             "python",
-#             "-m", "hw2.experiments",
-#             "run-exp",
-#             "-n", "exp1_4",
-#             "-K", "64", "128", "256",
-#             "-L", str(L),
-#             "-H", "100",
-#             "--early-stopping", "8",
-#             "-M", "resnet",
-#             "-s", "42",
-#             "-P", str(min(5, int(L/2)))
-#         ]
+# Iterate through all combinations of parameters
+for K in K_values:
+    for L in L_values:
+        # for P in P_values:
+            # for H in H_values:
+                # Construct the command
+        command = [
+            "srun",
+            "-c", "2",
+            "--gres=gpu:1",
+            "--pty",
+            "python",
+            "-m", "hw2.experiments",
+            "run-exp",
+            "-n", "exp1_4",
+            "-K", "64", "128", "256",
+            "-L", str(L),
+            "-H", "100",
+            "--early-stopping", "8",
+            "-M", "resnet",
+            "-s", "42",
+            "-P", str(min(5, int(L/2)))
+        ]
         
-#         # Execute the command
-#         print("Running command: {}".format(' '.join(command)))
-#         subprocess.call(command)
+        # Execute the command
+        print("Running command: {}".format(' '.join(command)))
+        subprocess.call(command)
